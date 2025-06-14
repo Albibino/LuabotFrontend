16eec5550bccb3586314dcfd8bcc1b108fb63625
William Wollert <williamwollert@gmail.com>
2025-06-14 19:04:38 -0300

    1.0 - Sistema inicial

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -27,15 +27,14 @@
                     @else
                         <a
                             href="{{ route('login') }}"
-                            class="inline-block px-5 py-1.5 dark:text-[#EDEDEC] text-[#1b1b18] border border-transparent hover:border-[#19140035] dark:hover:border-[#3E3E3A] rounded-sm text-sm leading-normal"
-                        >
+                            class="inline-block px-7 py-3.5 dark:text-[#EDEDEC] text-[#1b1b18] border-2 border-transparent hover:border-[#19140035] dark:hover:border-[#3E3E3A] rounded-lg text-lg leading-normal font-medium shadow-sm hover:shadow-md transition-all duration-200">
                             Entrar
                         </a>
 
                         @if (Route::has('register'))
                             <a
                                 href="{{ route('register') }}"
-                                class="inline-block px-5 py-1.5 dark:text-[#EDEDEC] border-[#19140035] hover:border-[#1915014a] border text-[#1b1b18] dark:border-[#3E3E3A] dark:hover:border-[#62605b] rounded-sm text-sm leading-normal">
+                                class="inline-block px-7 py-3.5 dark:text-[#EDEDEC] border-2 border-[#19140035] hover:border-[#1915014a] text-[#1b1b18] dark:border-[#3E3E3A] dark:hover:border-[#62605b] rounded-lg text-lg leading-normal font-medium shadow-sm hover:shadow-md transition-all duration-200">
                                 Registrar-se
                             </a>
                         @endif
@@ -45,16 +44,17 @@
         </header>
         
         <div class="flex items-center justify-center w-full transition-opacity opacity-100 duration-750 lg:grow starting:opacity-0">
-            <main class="flex max-w-[335px] w-full flex-col-reverse lg:max-w-4xl lg:flex-row">
-                <div class="text-[13px] leading-[20px] flex-1 p-6 pb-12 lg:p-20 bg-white dark:bg-[#161615] dark:text-[#EDEDEC] shadow-[inset_0px_0px_0px_1px_rgba(26,26,0,0.16)] dark:shadow-[inset_0px_0px_0px_1px_#fffaed2d] rounded-bl-lg rounded-br-lg lg:rounded-tl-lg lg:rounded-br-none">
+            <main class="flex max-w-[450px] w-full flex-col-reverse lg:max-w-6xl lg:flex-row lg:h-[500px]">
+                <div class="text-[14px] leading-[21px] flex-1 p-8 pb-16 lg:p-28 bg-white dark:bg-[#161615] dark:text-[#EDEDEC] shadow-[inset_0px_0px_0px_1px_rgba(26,26,0,0.16)] dark:shadow-[inset_0px_0px_0px_1px_#fffaed2d] rounded-bl-lg rounded-br-lg lg:rounded-tl-lg lg:rounded-br-none flex flex-col justify-center">
                     
-                    <h1 class="mb-1 font-medium">Seja bem vindo ao Luabot!</h1>
-                    <p class="mb-2 text-[#706f6c] dark:text-[#A1A09A]">Seu gerenciador do Luabot está aqui!.</p>
+                    <div>
+                        <h1 class="mb-3 font-medium text-xl lg:text-2xl">Seja bem vindo ao Luabot!</h1>
+                        <p class="mb-4 text-[#706f6c] dark:text-[#A1A09A] text-base">Seu gerenciador do Luabot está aqui! Gerencie suas configurações e explore todas as funcionalidades disponíveis.</p>
+                        
+                    </div>
                 </div>
 
-                
-                
-                <div class="bg-[#fff2f2] dark:bg-[#1D0002] relative lg:-ml-px -mb-px lg:mb-0 rounded-t-lg lg:rounded-t-none lg:rounded-r-lg aspect-[335/376] lg:aspect-auto w-full lg:w-[438px] shrink-0 overflow-hidden">
+                <div class="bg-[#fff2f2] dark:bg-[#1D0002] relative lg:-ml-px -mb-px lg:mb-0 rounded-t-lg lg:rounded-t-none lg:rounded-r-lg aspect-[335/376] lg:aspect-auto w-full lg:w-[550px] shrink-0 overflow-hidden">
                     <img src="{{ asset('luabot.png') }}" alt="Luabot" class="absolute inset-0 w-full h-full object-cover">
                 </div>
             </main>
